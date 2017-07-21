b56936279619f9bbc58e254d67105fa0c972d4b0
xiangkunisocket <30344712+xiangkunisocket@users.noreply.github.com>
2017-07-21 20:23:04 +0800

    Create help.c

---
+++ LICENSE.md/help.c
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: func.c
+	> Author: ECHO
+	> Mail: 1406451659@QQ.com 
+	> Created Time: 2017年04月05日 星期三 11时30分04秒
+ ************************************************************************/
+/************help函数************/
+void mohodump_help()
+{
+	printf("USAGE:[-i <ip>,-a <arp> ,-t <tcp> ,-u <udp>,-c <icmp> ,-p <port> ]\nUSAGE:[src host id,dsc host id,port id ,src or dst port id]\nUSAGE:[or ,and ,no]\n");
+}
