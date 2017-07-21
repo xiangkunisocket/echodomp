ae901516041b5eb11b892d6e080de9cb3efea5d8
xiangkunisocket <30344712+xiangkunisocket@users.noreply.github.com>
2017-07-21 20:25:08 +0800

    Create main.h

---
+++ LICENSE.md/main.h
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: main.h
+	> Author: ECHO
+	> Mail: 1406451659@QQ.com 
+	> Created Time: 2017年04月05日 星期三 10时50分00秒
+ ************************************************************************/
+#ifndef ECHODUMP_H
+#define ECHODUMP_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "pcap.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#endif
+
+#define MAXBYTECAPTURE 2048    //定义捕获数据包最大长度
+#define ARP_REQUEST 1
+#define ARP_REPLY 2
+#define TRUE 1
+#define FALSE 0
+
